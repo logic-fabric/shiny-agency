90b65e6806cc31d4783757accae8c184fedfc1df
logic-fabric <lowic@logic-fabric.fr>
2021-07-04 00:59:48 +0200

    style: complete CSS of Results page

--- src/pages/Results.jsx
+++ src/pages/Results.jsx
@@ -1,8 +1,11 @@
 import { useContext } from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 import ErrorMain from "../components/ErrorMain";
-import { SurveyContext } from "../utils/context/providers";
+import { SurveyContext, ThemeContext } from "../utils/context/providers";
 import { useFetch } from "../utils/hooks/useFetch";
+import colors from "../utils/style/colors";
 
 function setNeededSkillsFromUserAnswers(survey, surveyAnswers) {
   const neededSkills = new Set();
@@ -19,6 +22,7 @@ function setNeededSkillsFromUserAnswers(survey, surveyAnswers) {
 }
 
 function Results() {
+  const { theme } = useContext(ThemeContext);
   const { surveyAnswers } = useContext(SurveyContext);
   const { data, isDataLoading, error } = useFetch("../data/sample-survey.json");
   const survey = data;
@@ -37,10 +41,79 @@ function Results() {
     : setNeededSkillsFromUserAnswers(survey, surveyAnswers);
 
   return (
-    <main>
-      <h1>Les compétences dont vous avez besoin : {neededSkills}</h1>
-    </main>
+    <ResultsContainer $isDarkTheme={theme === "dark"}>
+      <ResultsTitle>
+        Les compétences dont vous avez besoin&nbsp;:{" "}
+        <NeededSkillsSpan $isDarkTheme={theme === "dark"}>
+          {neededSkills}
+        </NeededSkillsSpan>
+      </ResultsTitle>
+      <CallToActionContainer>
+        <CallToActionLink to="/freelances">
+          Découvrez nos profils
+        </CallToActionLink>
+      </CallToActionContainer>
+      <JobsDetails>
+        <JobTitle $isDarkTheme={theme === "dark"}>UI designer</JobTitle>
+        <JobDescription>
+          Le designer UI prend en charge les maquettes et graphismes du site.
+        </JobDescription>
+      </JobsDetails>
+    </ResultsContainer>
   );
 }
+
+const ResultsContainer = styled.main`
+  padding: 11rem 6rem;
+
+  background: ${(props) =>
+    props.$isDarkTheme ? `${colors.neutral700}` : `${colors.neutral100}`};
+`;
+
+const ResultsTitle = styled.h1`
+  max-width: 50rem;
+  margin: auto;
+
+  line-height: 1.5;
+  text-align: center;
+`;
+
+const NeededSkillsSpan = styled.span`
+  color: ${(props) =>
+    props.$isDarkTheme ? `${colors.neutral300}` : `${colors.primary500}`};
+`;
+
+const CallToActionContainer = styled.div`
+  text-align: center;
+`;
+
+const CallToActionLink = styled(Link)`
+  display: inline-block;
+
+  margin: 2rem;
+  padding: 0.5rem 4rem;
+  border-radius: 2rem;
+
+  color: white;
+  font-size: 1.1rem;
+  font-weight: 700;
+  text-decoration: none;
+
+  background: ${colors.primary500};
+`;
+
+const JobsDetails = styled.div`
+  max-width: 60rem;
+  margin: auto;
+`;
+
+const JobTitle = styled.h2`
+  color: ${(props) =>
+    props.$isDarkTheme ? `${colors.neutral300}` : `${colors.primary500}`};
+`;
+
+const JobDescription = styled.p`
+  font-size: 1.3rem;
+`;
 
 export default Results;
